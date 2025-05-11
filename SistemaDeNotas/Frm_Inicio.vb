606bcaa96f2953f2c6d90e435cd93c78949ddd59
Orlando <espadachinkinno@gmail.com>
2025-05-11 11:54:21 -0600

    Arregar progress bar de formulario de inicio

--- SistemaDeNotas/Frm_Inicio.vb
+++ SistemaDeNotas/Frm_Inicio.vb
@@ -1,18 +1,27 @@
 ﻿Public Class Frm_Inicio
     'medidor del tiempo
     Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
-        Static btecontenedor As Byte
-        If btecontenedor = 1 Then
+        If Pb_Tiempo.Value < Pb_Tiempo.Maximum Then
+            Pb_Tiempo.Value += 1
+        Else
+            Timer1.Stop()
+            Pb_Tiempo.Visible = False
+
+            ' Aquí puedes abrir el siguiente formulario o cerrar el actual
             Timer1.Stop()
             Timer1.Enabled = False
             Me.Hide()
             Frm_Login.Show()
-        Else
-            btecontenedor = btecontenedor + 1
         End If
     End Sub
     Private Sub Frm_Inicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Pb_Tiempo.Visible = True
+        Pb_Tiempo.Minimum = 0
+        Pb_Tiempo.Maximum = 100
+        Pb_Tiempo.Value = 0
+
+        Timer1.Interval = 1 ' Intervalo de 1 milisegundo
+        Timer1.Start()
     End Sub
 
     Private Sub Btn_Cerrar_Click(sender As Object, e As EventArgs) Handles Btn_Cerrar.Click
