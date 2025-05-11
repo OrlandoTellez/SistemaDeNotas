9e297eea7484356cb1f355ffff1be1992c9d8f4b
Orlando <espadachinkinno@gmail.com>
2025-05-10 21:43:15 -0600

    agregar datos del status strip, junto con su codigo mejorado

--- SistemaDeNotas/Frm_Principal.Designer.vb
+++ SistemaDeNotas/Frm_Principal.Designer.vb
@@ -23,18 +23,44 @@ Partial Class Frm_Principal
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Stb_Barra_Estado = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel3 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Stb_Barra_Estado.SuspendLayout()
         Me.SuspendLayout()
         '
         'Stb_Barra_Estado
         '
         Me.Stb_Barra_Estado.BackColor = System.Drawing.Color.FromArgb(CType(CType(37, Byte), Integer), CType(CType(46, Byte), Integer), CType(CType(59, Byte), Integer))
         Me.Stb_Barra_Estado.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.Stb_Barra_Estado.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripStatusLabel2, Me.ToolStripStatusLabel3})
         Me.Stb_Barra_Estado.Location = New System.Drawing.Point(0, 416)
         Me.Stb_Barra_Estado.Name = "Stb_Barra_Estado"
         Me.Stb_Barra_Estado.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional
         Me.Stb_Barra_Estado.Size = New System.Drawing.Size(756, 22)
         Me.Stb_Barra_Estado.TabIndex = 1
         Me.Stb_Barra_Estado.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.ForeColor = System.Drawing.Color.White
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(72, 17)
+        Me.ToolStripStatusLabel1.Text = "Datos usuario"
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.ForeColor = System.Drawing.Color.White
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(40, 17)
+        Me.ToolStripStatusLabel2.Text = "Fecha:"
+        '
+        'ToolStripStatusLabel3
+        '
+        Me.ToolStripStatusLabel3.ForeColor = System.Drawing.Color.White
+        Me.ToolStripStatusLabel3.Name = "ToolStripStatusLabel3"
+        Me.ToolStripStatusLabel3.Size = New System.Drawing.Size(33, 17)
+        Me.ToolStripStatusLabel3.Text = "Hora:"
         '
         'Frm_Principal
         '
@@ -47,10 +73,15 @@ Partial Class Frm_Principal
         Me.Name = "Frm_Principal"
         Me.Text = "Form1"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.Stb_Barra_Estado.ResumeLayout(False)
+        Me.Stb_Barra_Estado.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Stb_Barra_Estado As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel3 As ToolStripStatusLabel
 End Class
--- SistemaDeNotas/Frm_Principal.vb
+++ SistemaDeNotas/Frm_Principal.vb
@@ -1,5 +1,11 @@
 ﻿Public Class Frm_Principal
+    ' Variables de usuario y sistema
+    Private SISTEMA As String = "Sistema de Gestión"
+    Private LOGIN_USER As String = "usuario01"
+    Private NOMBRE_USER As String = "Juan Pérez"
+    Private TIPO_USER As String = "Administrador"
     Private Sub Frm_Principal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
         'visualizando el formulario Encabezado
         Frm_Encabezado.MdiParent = Me
         Frm_Encabezado.Show()
@@ -16,10 +22,16 @@
         Frm_Piedepagina.MdiParent = Me
         Frm_Piedepagina.Show()
 
-        'Me.Text = SISTEMA
-        'Me.Stb_Barra_Estado.Items(0).Text = Space(0) & "USUARIO: " & LOGIN_USER & Space(15) &
-        '    "NOMBRE DEL USUARIO: " & NOMBRE_USER & Space(15) & "TIPO DE CUENTA: " & TIPO_USER
-        'Me.Stb_Barra_Estado.Items(1).Text = UCase(Format(Now, "SHORT DATE"))
-        'Me.Stb_Barra_Estado.Items(2).Text = UCase(Format(Now, "hh:mm: tt"))
+        Me.Text = SISTEMA
+        ' Asegúrate de que haya 3 elementos en el StatusStrip
+        If Me.Stb_Barra_Estado.Items.Count >= 3 Then
+            Me.Stb_Barra_Estado.Items(0).Text = "USUARIO: " & LOGIN_USER & Space(5) &
+                                                "NOMBRE: " & NOMBRE_USER & Space(5) &
+                                                "TIPO: " & TIPO_USER
+
+            Me.Stb_Barra_Estado.Items(1).Text = UCase(Format(Now, "Short Date"))
+            Me.Stb_Barra_Estado.Items(2).Text = UCase(Format(Now, "hh:mm tt"))
+        End If
     End Sub
+
 End Class
